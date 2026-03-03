2603d2b0b0ff800f9f5b553aee83efe51773ca4a
Alas de Regreso RD <scarllett77@gmail.com>
2026-03-03 16:19:49 +0000

    Instalación de PostCSS: Activando la brocha de estilos

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,14 @@
 import './globals.css'
 
+export const metadata = {
+  title: 'Alas de Regreso RD',
+  description: 'Estructura tecnológica para el regreso a la plenitud',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   )
 }
